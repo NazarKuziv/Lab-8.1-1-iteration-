53dd60485f9e580e3937b60d4c7c1d7d1da9f4b4
Nazar Kuziv <nazar123kuziv@gmail.com>
2021-03-07 10:25:39 +0200

    first commit

---
+++ Project 8.1-1(ітерація).cpp
@@ -0,0 +1,40 @@
+﻿// Lab_8_1-1.cpp
+// < Кузів Назар >
+// Лабораторна робота № 8.1
+// Пошук та заміна символів у літерному рядку
+// Варіант 6
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+/*Дано літерний рядок, який містить послідовність символів s0, …, sn, … .
+ Вияснити, чи є серед цих символів пара сусідніх букв “SQ” або ”QS”.
+*/
+
+bool Include(const char* s)
+{
+
+	for (int j = 0; s[j] != '\0'; j++)
+		if (( s[j]=='S' && s[j + 1]=='Q') || (s[j] == 'Q' && s[j + 1] == 'S'))
+			return true;
+
+	return false;
+}
+
+int main()
+{
+	char s[101]; //Оголошення літерного рядка
+	
+
+	cout << "Enter string:" << endl;
+	cin.getline(s, 100);
+
+	if (Include(s))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+
+	return 0;
+}
